a1f691eafa22b23733c90ffe5bbc9b0a4a428c34
Daniela <dtricolici12345@gmail.com>
2024-05-11 14:26:42 +0200

    The order processing with backend transmission has been added.

--- sneakers/src/components/Drawer.jsx
+++ sneakers/src/components/Drawer.jsx
@@ -1,10 +1,44 @@
 /* eslint-disable react/prop-types */
-
+import { useState, useContext } from "react";
 import Info from "./Info";
+import axios from "axios";
+import AppContext from "../components/context";
 
-
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 function Drawer({ onClose, onRemove, items }) {
+  const { cartItems, setCartItems } = useContext(AppContext);
+
+  const [orderId, setOrderId] = useState(null);
+  const [isOrderComplete, setIsOrderComplete] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClickOrder = async () => {
+    try {
+      setIsLoading(true);
+      const { data } = await axios.post(
+        "https://6628e35a54afcabd07375526.mockapi.io/orders",
+        {
+          items: cartItems,
+        }
+      );
+      setOrderId(data.id);
+      setIsOrderComplete(true);
+      setCartItems([]);
+
+      for (let i = 0; i < cartItems.length; i++) {
+        const item = cartItems[i];
+        await axios.delete(
+          "https://6628e35a54afcabd07375526.mockapi.io/cart" + item.id
+        );
+        await delay(1000);
+      }
+    } catch (error) {
+      alert("Error during creation :(");
+    }
+    setIsLoading(false);
+  };
+
   return (
     <div className="overlay">
       <div className="drawer d-flex flex-column">
@@ -19,26 +53,29 @@ function Drawer({ onClose, onRemove, items }) {
         </h2>
 
         {items.length > 0 ? (
-           <div className="d-flex flex-column flex">
+          <div className="d-flex flex-column flex">
             <div className="items flex">
-            {items.map((obj) => (
-  <div key={obj.id} className="cartItem d-flex align-center mb-20">
-    <div
-      style={{ backgroundImage: `url(${obj.imageUrl})` }}
-      className="cartItemImg"
-    ></div>
-    <div className="mr-20">
-      <p className="mb-5">{obj.title}</p>
-      <b>{obj.price} EUR.</b>
-    </div>
-    <img
-      onClick={() => onRemove(obj.id)}
-      className="removeBtn"
-      src="/img/delete.svg"
-      alt="Remove"
-    />
-  </div>
-))}
+              {items.map((obj) => (
+                <div
+                  key={obj.id}
+                  className="cartItem d-flex align-center mb-20"
+                >
+                  <div
+                    style={{ backgroundImage: `url(${obj.imageUrl})` }}
+                    className="cartItemImg"
+                  ></div>
+                  <div className="mr-20">
+                    <p className="mb-5">{obj.title}</p>
+                    <b>{obj.price} EUR.</b>
+                  </div>
+                  <img
+                    onClick={() => onRemove(obj.id)}
+                    className="removeBtn"
+                    src="/img/delete.svg"
+                    alt="Remove"
+                  />
+                </div>
+              ))}
             </div>
             <div className="cartTotalBlock">
               <ul>
@@ -53,16 +90,29 @@ function Drawer({ onClose, onRemove, items }) {
                   <b>10,74$</b>
                 </li>
               </ul>
-              <button className="greenButton">
+              <button
+                disabled={isLoading}
+                onClick={onClickOrder}
+                className="greenButton"
+              >
                 Checkout <img src="/img/arrow.svg" alt="Arrow" />
               </button>
             </div>
           </div>
         ) : (
-      <Info
-      title=""
-      description=""
-      image=""/>
+          <Info
+            title={isOrderComplete ? "Order is processed!" : "Cart empty"}
+            description={
+              isOrderComplete
+                ? `Your order #${orderId} will soon be handed over to courier delivery.`
+                : "Add at least one pair of sneakers to complete the order."
+            }
+            image={
+              isOrderComplete
+                ? "/img/complete-order.jpg"
+                : "/img/empty-cart.jpg"
+            }
+          />
         )}
       </div>
     </div>
--- sneakers/src/components/Info.jsx
+++ sneakers/src/components/Info.jsx
@@ -5,7 +5,7 @@ import AppContext from '../components/context';
 const Info = ({ title, image, description }) => {
     const { setCartOpened } = useContext(AppContext);
     return (
-        <div className="cartEmpty d-flex align-center justify-center flex-column flex">
+        <div className="cartEmpty  d-flex align-center justify-center flex-column flex">
             <img
                 className="mb-20"
                 width="120px"
@@ -14,7 +14,7 @@ const Info = ({ title, image, description }) => {
                 alt="Empty"
             />
             <h2>{title}</h2>
-            <p className="opacity-6">
+            <p className="opacity-6 mb-40">
                 {description}
             </p>
             <button onClick={() => setCartOpened(false)} className="greenButton">
